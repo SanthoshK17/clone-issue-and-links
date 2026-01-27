0d6d08c1d1a2da6017050442b5a447a4bf4ae51e
Santhosh Kumar <santhosh131517@gmail.com>
2026-01-28 01:42:54 +0530

    index.js v1 from gen code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import Resolver from '@forge/resolver';
+import { requestJira } from '@forge/api';
 
 const resolver = new Resolver();
 
-resolver.define('getText', (req) => {
-  console.log(req);
+resolver.define('cloneIssue', async ({ context }) => {
+  const issueKey = context.extension.issue.key;
 
-  return 'Hello, world!';
+  const res = await requestJira(`/rest/api/3/issue/${issueKey}`);
+  const issue = await res.json();
+
+  return {
+    original: issue.key,
+    summary: issue.fields.summary
+  };
 });
 
 export const handler = resolver.getDefinitions();
